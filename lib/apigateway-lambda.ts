61e5a5b7737d07ae3944ec95e201c385ab0bb3ce
tettz <tettz@proton.me>
2023-03-04 20:55:44 +0900

    [feat] impl dynamic configurable functionality.

--- lib/apigateway-lambda.ts
+++ lib/apigateway-lambda.ts
@@ -1,89 +1,118 @@
-import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib'
+import { StackProps } from 'aws-cdk-lib'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigwv2 from '@aws-cdk/aws-apigatewayv2-alpha'
-import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
+import {
+  HttpLambdaIntegration,
+  HttpLambdaIntegrationProps,
+  HttpUrlIntegration,
+  HttpUrlIntegrationProps,
+} from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
 import { Construct } from 'constructs'
 
-interface ApiGatewayLambdaProps extends StackProps {
-  path?: string
-  lambdaProps?: Partial<lambda.FunctionProps>
+interface ApiBackend {
+  id: string
+  existingLambda?: lambda.IFunction
+  lambdaProps?: lambda.FunctionProps
+  lambdaIntegrationProps?: HttpLambdaIntegrationProps
+  url?: string
+  urlIntegrationProps?: HttpUrlIntegrationProps
+}
+
+interface MethodMappings {
+  [methods: string]: ApiBackend
+}
+
+export interface ApiGatewayLambdaProps extends StackProps {
+  httpApiProps?: apigwv2.HttpApiProps
+  defaultApi?: ApiBackend
+  apiMappings?: {
+    [path: string]: MethodMappings
+  }
 }
 
 export class ApiGatewayLambda extends Construct {
-  public readonly defaultApi: lambda.Function
-  public readonly deleteApi: lambda.Function
-  public readonly getApi: lambda.Function
+  public readonly apiMappings: {
+    [id: string]: lambda.Function | lambda.IFunction
+  }
   public readonly httpApi: apigwv2.HttpApi
-  public readonly postApi: lambda.Function
-  public readonly putApi: lambda.Function
 
   public constructor(scope: Construct, id: string, props?: ApiGatewayLambdaProps) {
     super(scope, id)
 
-    const lambdaProps: Partial<lambda.FunctionProps> = {
-      architecture: lambda.Architecture.X86_64,
-      environment: {
-        REGION: Stack.of(this).region,
-        ...(props?.lambdaProps?.environment ?? {}),
-      },
-      ...(props?.lambdaProps ?? {}),
+    this.apiMappings = {}
+
+    const createIntegration = (backend: ApiBackend): apigwv2.HttpRouteIntegration => {
+      const { id, existingLambda, lambdaProps, lambdaIntegrationProps, url, urlIntegrationProps } =
+        backend
+      let ret: apigwv2.HttpRouteIntegration | undefined
+      if (existingLambda) {
+        if (lambdaProps || url) {
+          throw new Error(
+            'Invalid ApiGatewayLambdaProps: you should provide only one of `existingLambda`, `lambdaProps`, or `url`.'
+          )
+        }
+        this.apiMappings[id] = existingLambda
+        ret = new HttpLambdaIntegration(id, existingLambda, lambdaIntegrationProps)
+      }
+      if (lambdaProps) {
+        if (existingLambda || url) {
+          throw new Error(
+            'Invalid ApiGatewayLambdaProps: you should provide only one of `existingLambda`, `lambdaProps`, or `url`.'
+          )
+        }
+        const func = new lambda.Function(this, `${id}Function`, lambdaProps)
+        this.apiMappings[id] = func
+        ret = new HttpLambdaIntegration(id, func, lambdaIntegrationProps)
+      }
+      if (url) {
+        if (existingLambda || lambdaProps) {
+          throw new Error(
+            'Invalid ApiGatewayLambdaProps: you should provide only one of `existingLambda`, `lambdaProps`, or `url`.'
+          )
+        }
+        ret = new HttpUrlIntegration(id, url, urlIntegrationProps)
+      }
+      if (ret == null) {
+        throw new Error(
+          'Invalid ApiGatewayLambdaProps: you should provide one of `existingLambda`, `lambdaProps`, or `url`.'
+        )
+      }
+      return ret
     }
 
-    this.defaultApi = new lambda.Function(this, 'DefaultApi', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('./bundle/defaultApi'),
-      handler: 'index.handler',
-      ...lambdaProps,
-    })
-    this.getApi = new lambda.Function(this, 'GetApi', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('./bundle/getApi'),
-      handler: 'index.handler',
-      ...lambdaProps,
-    })
-    this.postApi = new lambda.Function(this, 'PostApi', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('./bundle/postApi'),
-      handler: 'index.handler',
-      ...lambdaProps,
-    })
-    this.putApi = new lambda.Function(this, 'PutApi', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('./bundle/putApi'),
-      handler: 'index.handler',
-      ...lambdaProps,
-    })
-    this.deleteApi = new lambda.Function(this, 'DeleteApi', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('./bundle/deleteApi'),
-      handler: 'index.handler',
-      ...lambdaProps,
-    })
+    let defaultApi: apigwv2.HttpRouteIntegration | undefined
+    if (props?.defaultApi != null) {
+      defaultApi = createIntegration(props.defaultApi)
+    }
 
-    const path = props?.path ?? '/api'
     const httpApi = new apigwv2.HttpApi(this, 'HttpApi', {
-      defaultIntegration: new HttpLambdaIntegration('DefaultIntegration', this.defaultApi),
+      ...(props?.httpApiProps ?? {}),
+      ...(defaultApi
+        ? {
+            defaultIntegration: defaultApi,
+          }
+        : {}),
     })
     this.httpApi = httpApi
-    httpApi.addRoutes({
-      path,
-      methods: [apigwv2.HttpMethod.GET],
-      integration: new HttpLambdaIntegration('GetIntegration', this.getApi),
-    })
-    httpApi.addRoutes({
-      path,
-      methods: [apigwv2.HttpMethod.POST],
-      integration: new HttpLambdaIntegration('PostIntegration', this.postApi),
-    })
-    httpApi.addRoutes({
-      path,
-      methods: [apigwv2.HttpMethod.PUT],
-      integration: new HttpLambdaIntegration('PutIntegration', this.putApi),
-    })
-    httpApi.addRoutes({
-      path,
-      methods: [apigwv2.HttpMethod.DELETE],
-      integration: new HttpLambdaIntegration('DeleteIntegration', this.deleteApi),
-    })
+
+    const apiMappings = props?.apiMappings ?? {}
+    for (const path in apiMappings) {
+      const api = apiMappings[path]
+      for (const methodNames in api) {
+        const methods = methodNames
+          .split('|')
+          .map((m) => m.trim().toUpperCase())
+          .filter((m) => Object.values(apigwv2.HttpMethod).includes(m as apigwv2.HttpMethod))
+        if (methods.length === 0) {
+          throw new Error() //FIXME
+        }
+        const integration = createIntegration(api[methodNames])
+        httpApi.addRoutes({
+          path: path,
+          methods: methods as apigwv2.HttpMethod[],
+          integration: integration,
+        })
+      }
+    }
   }
 }
--- stack/cdk-basics-apigateway-lambda-stack.ts
+++ stack/cdk-basics-apigateway-lambda-stack.ts
@@ -1,4 +1,5 @@
-import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib'
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
 import { ApiGatewayLambda } from '../lib/apigateway-lambda'
 
@@ -6,7 +7,67 @@ export class CdkBasicsApiGatewayLambdaStack extends Stack {
   public constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const apiGatewayLambda = new ApiGatewayLambda(this, 'ApiGatewayLambda')
+    const environment = {
+      REGION: Stack.of(this).region,
+    }
+    const apiGatewayLambda = new ApiGatewayLambda(this, 'ApiGatewayLambda', {
+      defaultApi: {
+        id: 'DefaultApi',
+        lambdaProps: {
+          architecture: lambda.Architecture.X86_64,
+          runtime: lambda.Runtime.NODEJS_18_X,
+          code: lambda.Code.fromAsset('./bundle/defaultApi'),
+          handler: 'index.handler',
+          environment,
+        },
+      },
+      apiMappings: {
+        '/query': {
+          'get|head': {
+            id: 'GetApi',
+            lambdaProps: {
+              architecture: lambda.Architecture.X86_64,
+              runtime: lambda.Runtime.NODEJS_18_X,
+              code: lambda.Code.fromAsset('./bundle/getApi'),
+              handler: 'index.handler',
+              environment,
+            },
+          },
+        },
+        '/mutate': {
+          post: {
+            id: 'PostApi',
+            lambdaProps: {
+              architecture: lambda.Architecture.X86_64,
+              runtime: lambda.Runtime.NODEJS_18_X,
+              code: lambda.Code.fromAsset('./bundle/postApi'),
+              handler: 'index.handler',
+              environment,
+            },
+          },
+          put: {
+            id: 'PutApi',
+            lambdaProps: {
+              architecture: lambda.Architecture.X86_64,
+              runtime: lambda.Runtime.NODEJS_18_X,
+              code: lambda.Code.fromAsset('./bundle/putApi'),
+              handler: 'index.handler',
+              environment,
+            },
+          },
+          delete: {
+            id: 'DeleteApi',
+            lambdaProps: {
+              architecture: lambda.Architecture.X86_64,
+              runtime: lambda.Runtime.NODEJS_18_X,
+              code: lambda.Code.fromAsset('./bundle/deleteApi'),
+              handler: 'index.handler',
+              environment,
+            },
+          },
+        },
+      },
+    })
 
     new CfnOutput(this, 'ApiEndpoint', {
       value: apiGatewayLambda.httpApi.apiEndpoint,
